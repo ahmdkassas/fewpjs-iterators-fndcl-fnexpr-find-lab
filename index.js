1d7719222812f994de1014186aca6f4410bea86c
ahmed <ahm.gmal@gmail.com>
2021-04-05 23:28:35 -0400

    Completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+
+function superbowlWin([...record]){
+    let winYear = record.find(isWin);
+    if(typeof(winYear) === "undefined"){
+        return winYear;
+    }
+    else{
+        return winYear.year;
+    }
+}
+
+function isWin(record){
+    return record.result === "W"
+}
